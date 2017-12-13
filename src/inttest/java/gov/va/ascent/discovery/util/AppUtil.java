d27d1789f96f96ffc9b33ecffbb2a8c480c578a2
suchitharavi <sravi@ip-192-168-47-70.us-gov-west-1.compute.internal>
2017-12-13 12:11:01 -0500

    Parameter changes.

---
+++ src/inttest/java/gov/va/ascent/discovery/util/AppUtil.java
@@ -0,0 +1,42 @@
+package gov.va.ascent.discovery.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import gov.va.ascent.test.framework.service.RESTConfigService;
+import gov.va.ascent.test.framework.service.VaultService;
+import gov.va.ascent.test.framework.util.AppConstants;
+import gov.va.ascent.test.framework.util.RESTUtil;
+
+public class AppUtil {
+
+	private static final Pattern urlPattern = Pattern.compile("(http|https)://([A-Za-z0-9\\-\\.]+)(:(\\d+))$");
+	
+	private AppUtil() {
+		
+	}
+	
+	public static String getBaseURL()  {
+		RESTConfigService restConfig =  RESTConfigService.getInstance();
+		String baseURL =  restConfig.getPropertyName("baseURL", true);
+		
+		String vaultToken = System.getProperty(AppConstants.VAULT_TOKEN_PARAM_NAME);
+		if(vaultToken != null && vaultToken != "") {
+			String jsonResponse = VaultService.getVaultCredentials(vaultToken);
+			RESTUtil restUtil = new RESTUtil();
+			String userName = restUtil.parseJSON(jsonResponse, "data.username");
+			String password = restUtil.parseJSON(jsonResponse, "data.password"); 
+			
+			final Matcher m = urlPattern.matcher(baseURL);
+			if(!m.matches()) 
+				throw new RuntimeException("Invalid base url!");	
+			final String protocol = m.group(1).toLowerCase();
+			final String host     = m.group(2);
+			final String port     = m.group(3);
+			String finalUrl = protocol + "://" + userName + ":" + password + "@" + host + port;
+			return finalUrl;
+		}
+		System.out.println(baseURL);
+		return baseURL;
+	}
+}
